e8d6f278b0b4221e1f90d9a0bbbff6b6726fbf00
Patricia Padilha Coleman <patricia.p.coleman@gmail.com>
2019-02-03 20:01:22 +0000

    Automatically backed up by Learn

--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,43 @@
 class Owner
-  # code goes here
+  attr_reader :name, :species
+  attr_accessor :pets
+  @@all = []
+  @@count = 0
+  
+  def initialize(name)
+    
+    @name = name
+    @species = "human"
+    @pets = {fishes:[], cats:[], dogs:[] }
+    @@all << self
+    @@count +=1
+    
+  end
+  
+  def buy_fish(name)
+    
+    self.pets[fishes] << Fish.new(name)
+    
+    
+  end
+  
+  def self.all
+  
+    @@all
+  
+  end
+  
+  def self.count
+    
+    @@count
+    
+  end
+  
+  def self.reset_all
+    
+    @@count = 0
+    
+  end
+  
+ 
 end
